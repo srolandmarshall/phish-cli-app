11fde6bf5b8a7eb12d2fca5f626d2d32f258ecdd
srolandmarshall <samuel.r.marshall@gmail.com>
2017-07-20 19:28:23 -0400

    added instance variables and @@all for Show

--- lib/show.rb
+++ lib/show.rb
@@ -1,3 +1,17 @@
 class Show
 
+  attr_accessor :venue, :city, :day, :month, :year, :date, :num_sets, :setlist, :notes, :rating, :jams
+
+  #setlist should be a hash with key value pairs of arrays of Songs.
+
+  #other shows on this date??
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+
+
 end
