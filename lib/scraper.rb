1651d1d4f5b77de01f180b96411d8712e5338788
srolandmarshall <samuel.r.marshall@gmail.com>
2017-07-19 22:32:46 -0400

    started Song class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,36 @@
 require_relative '../config/environment.rb'
 
 class Scraper
-  TOUR_PAGE = "http://phish.net/tour"
-  page = Nokogiri::HTML(open(TOUR_PAGE))
-  cells = page.css("td")
-  i=0
-  while i < cells.length
-    name = cells[i].text
-    link = cells[i].css("a").first["href"]
-    year = cells[i+1]
-    shows = cells[i+2].text.split(" ")[0].to_i
-    Tour.new(name,link,year,shows)
-    i+=3
+  TOURS_PAGE = "http://phish.net/tour/"
+  SONGS_PAGE = "http://phish.net/song/"
+
+  def self.scrape_tours
+    page = Nokogiri::HTML(open(TOURS_PAGE))
+    cells = page.css("td")
+    i=0
+    while i < cells.length
+      name = cells[i].text
+      link = cells[i].css("a").first["href"]
+      year = cells[i+1]
+      shows = cells[i+2].text.split(" ")[0].to_i
+      Tour.new(name,link,year,shows)
+      i+=3
+    end
+    binding.pry
   end
-  binding.pry
+
+  def self.scrape_songs
+    page = Nokogiri::HTML(open(SONGS_PAGE))
+    cells = page.css("td")
+    i=0
+    while i < cells.length
+      name = cells[i].text
+      link = cells[i].css("a").first["href"]
+      year = cells[i+1]
+      shows = cells[i+2].text.split(" ")[0].to_i
+      Tour.new(name,link,year,shows)
+      i+=3
+    end
+  end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,21 @@
+require_relative '../config/environment.rb'
 class Song
+
+  @@all = []
+  attr_accessor :name, :original_artist, :times_played, :debut_date, :last_played, :gap
+
+  def initialize(name="",original_artist="",times_played=0,debut="", last="", gap=0)
+    @name = name
+    @original_artist = original_artist
+    @times_played = times_played
+    @debut_date = debut
+    @last_played = last
+    @gap = gap
+  end
+
+  def self.all
+    @@all
+  end
+
 
 end
